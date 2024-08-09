a97dbb28d956318fccc87ad1a2e8a53e0aa2a148
Pai22 <paisrp22@gmail.com>
2024-08-09 20:50:29 +0700

    Add header,content,footer
    Add header,content,footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './style.css'
 import Calendar from './class-components';
+import { Header, Content, Footer } from './func-component';
 
 function App(){
   
   return (
     <>
+    <Header/>
+    <p><center><Calendar/></center></p>
     <h2>DeveloperThai.com</h2>
     <div className='title'>
       React & React Native
     </div>
-
-    <p><center><Calendar/></center></p>
-    
+    <Content/>
+    <Footer/>
     </>
   )
 }
